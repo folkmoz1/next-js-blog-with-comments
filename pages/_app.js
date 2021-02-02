2b19c8c3463cb7ca4c126b2d15c8ad8e26cbdb25
folkmoz1 <63143147+folkmoz1@users.noreply.github.com>
2021-02-02 23:05:41 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+       <Component {...pageProps} />
+       <style jsx global>{`#__next{ overflow: hidden }`}</style>
+    </>
+  )
 }
 
 export default MyApp
